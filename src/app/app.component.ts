6a6f13234bd5ae799e8c3188c9f5a8b36629eff7
Giada Gabriele <giadagabriele@protonmail.com>
2022-08-02 15:13:38 +0200

    added scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,41 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, OnInit, Inject, HostListener } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'myApp';
   @HostBinding('class') className = '';
 
   toggleControl = new FormControl(false);
-
+  windowScrolled: boolean = true;
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+      if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+          this.windowScrolled = true;
+      } 
+     else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+          this.windowScrolled = false;
+      }
+  }
+  scrollToTop() {
+      (function smoothscroll() {
+          var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+          if (currentScroll > 0) {
+              window.requestAnimationFrame(smoothscroll);
+              window.scrollTo(0, currentScroll - (currentScroll / 8));
+          }
+      })();
+  }
   ngOnInit() : void {
     this.toggleControl.valueChanges.subscribe(val => {
       this.className = val ? 'darkMode' : '';
     })
   }
 }
+
